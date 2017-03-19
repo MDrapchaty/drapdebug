7464719f5ce2e5df11ed7a4a01260196a7e87335
Matt Drapchaty <Drapchaty@comcast.net>
2017-03-19 01:16:15 -0600

    fixed critical error when debug is off

--- src/debug.js
+++ src/debug.js
@@ -1,15 +1,17 @@
 
 const fs = require('fs'); // require fs for logging
 
-if (process.env.DEBUG === 'true') {  // if .env var DEBUG=true run function
+  // if .env var DEBUG=true run function
 // function for debug
   function debug(msg) {
 // creates log info that is logged to logs/log.log
-    const date = new Date();
-    const logMsg = date + ', ' + msg + '\n\n';
-    console.log(msg);
-   }
+   	if (process.env.DEBUG === 'true') {
+      const date = new Date();
+      const logMsg = date + ', ' + msg + '\n\n';
+      console.log(msg);
+    }
+  }
 
    exports.debug = debug;
 
- }
+ 
