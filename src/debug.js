7a4d0245bb3b73ca7e4a5869e82569036ea92f0a
Matt Drapchaty <Drapchaty@comcast.net>
2017-03-20 07:11:41 -0600

    codeship test

--- src/debug.js
+++ src/debug.js
@@ -4,7 +4,7 @@ const fs = require('fs'); // require fs for logging
   // if .env var DEBUG=true run function
 // function for debug
   function debug(msg) {
-// Creates log info that is logged to logs/log.log
+// creates log info that is logged to logs/log.log
    	if (process.env.DEBUG === 'true') {
       const date = new Date();
       const logMsg = date + ', ' + msg + '\n\n';
