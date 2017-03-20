145fb319867a63fefe6548dc4ab9d6e4fd1607e5
Kenny Baustert <kdbaustert@fullsail.edu>
2017-03-20 11:57:52 -0400

    added Warning, error and success

--- src/debug.js
+++ src/debug.js
@@ -1,17 +1,30 @@
 
 const fs = require('fs'); // require fs for logging
+const colors = require('colors');
+
+colors.setTheme({
+  success: 'green',
+  warn: 'yellow',
+  error: 'red'
+});
 
   // if .env var DEBUG=true run function
 // function for debug
+const date = new Date();
+const logMsg = date + ', ' + msg + '\n\n';
+
   function debug(msg) {
-// Creates log info that is logged to logs/log.log
    	if (process.env.DEBUG === 'true') {
-      const date = new Date();
-      const logMsg = date + ', ' + msg + '\n\n';
-      console.log(msg);
+      if (type == 'warn') {
+      // Warnings
+        console.warn(colors.warn(msg));
+    } else if (type == 'error') {
+      // Errors
+        console.error(colors.error(msg));
+    } else {
+      // All other logs will be in green
+        console.log(colors.success(msg));
     }
   }
-
+}
    exports.debug = debug;
-
- 
